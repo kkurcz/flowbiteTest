e4611dbb3d012a78a4ccb4273415c94700eaa76d
Kevin Kurczseitz <kevinkurczseitz@icloud.com>
2022-02-08 11:52:40 +0100

    add flowbite

---
+++ config/tailwind.config.js
@@ -0,0 +1,26 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: [
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+    './app/views/**/*',
+    // flowbite content node module
+    "./node_modules/flowbite/**/*.js"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/typography'),
+    // flowbite plugin
+    require('flowbite/plugin')
+
+  ]
+}
